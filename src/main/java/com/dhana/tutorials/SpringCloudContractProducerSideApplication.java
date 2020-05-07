a4e6161183454671e7bfaa9cf6901ff10d5751f2
Dhanasekaran Subramani <dhanasekaran.subramani@rabobank.nl>
2020-05-07 15:17:48 +0530

    initial project with dependecy

---
+++ src/main/java/com/dhana/tutorials/SpringCloudContractProducerSideApplication.java
@@ -0,0 +1,13 @@
+package com.dhana.tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudContractProducerSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudContractProducerSideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhana/tutorials/SpringCloudContractProducerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhana.tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudContractProducerSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
