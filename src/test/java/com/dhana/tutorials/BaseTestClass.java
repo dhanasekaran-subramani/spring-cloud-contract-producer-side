6e362f47c0f4a25fe8af4462179eccb074c1fcb3
Dhanasekaran Subramani <dhanasekaran.subramani@rabobank.nl>
2020-05-07 18:55:08 +0530

    Add BaseTestClass and configure in pom file

---
+++ src/test/java/com/dhana/tutorials/BaseTestClass.java
@@ -0,0 +1,14 @@
+package com.dhana.tutorials;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+
+public class BaseTestClass {
+
+    @BeforeEach
+    public void setup() {
+        RestAssuredMockMvc.standaloneSetup(new FraudController());
+    }
+}
+
+
